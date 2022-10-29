f6c47017402a73c7df0a303eb61d33765eb7f860
Oiseh Omokheoa <oisehzeme@gmail.com>
2022-10-29 15:46:23 +0100

    views changes

--- restapi/views.py
+++ restapi/views.py
@@ -20,5 +20,5 @@ def student_list(request):
     if request.method == 'GET':
         students = student.objects.all()
         serializer = studentSerializer(students, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data[0])
 
