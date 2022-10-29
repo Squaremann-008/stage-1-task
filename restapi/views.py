64cbf15f20beb3790e48927dad5f29e128e44719
Oiseh Omokheoa <oisehzeme@gmail.com>
2022-10-29 16:13:54 +0100

    cleanup

--- restapi/views.py
+++ restapi/views.py
@@ -9,14 +9,7 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view(['GET'])
 def student_list(request):
-    #students = student.objects.all()
-    #serializer = studentSerializer(students, many=True)
-    #response = JsonResponse(serializer.data, safe=False,)
-    #response['Access-Control-Allow-Origin'] = '*'
-    #response['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-    #response['Access-Control-Max-Age'] = '1000'
-    #response['Access-Control-Allow-Headers']= 'X-Requested-With, Content-type'
-    #return response
+    
     if request.method == 'GET':
         students = student.objects.all()
         serializer = studentSerializer(students, many=True)
